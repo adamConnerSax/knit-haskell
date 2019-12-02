80a438a939e4bafa91fa1afd6767fdb6e5f1e204
adamConnerSax <adam_conner_sax@yahoo.com>
2019-12-01 20:03:03 -0500

    Working on caching

--- src/Knit/Report/Cache.hs
+++ src/Knit/Report/Cache.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE PolyKinds            #-}
 {-# LANGUAGE Rank2Types           #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 {-# LANGUAGE TypeApplications     #-}
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -21,10 +22,16 @@ This module adds types, combinators and knit functions for using knit-haskell wi
 <https://github.com/adamConnerSax/knit-haskell/tree/master/examples Examples> are available, and might be useful for seeing how all this works.
 -}
 module Knit.Report.Cache
-  ( knitStore
-  , knitRetrieve
-  , knitRetrieveOrMake
-  , knitClear
+  (
+    -- * Interface Types
+    CacheHolder
+    -- * Combinators
+  , cacheAction
+  , useCached
+  , store
+  , retrieve
+  , retrieveOrMake
+  , clear
   )
 where
 
@@ -48,6 +55,22 @@ import qualified System.IO.Error               as IE
 import qualified Polysemy                      as P
 import qualified Polysemy.Error                as P
 
+type KnitCache = C.AtomicCache IE.IOError T.Text BS.ByteString
+
+data CacheHolder r a where
+  Made :: a -> CacheHolder r a
+  RetrieveOrMake :: S.Serialize b => T.Text -> P.Sem r b -> (b -> a) -> CacheHolder r a
+
+cacheAction
+  :: S.Serialize b => T.Text -> P.Sem r b -> (b -> a) -> CacheHolder r a
+cacheAction = RetrieveOrMake
+
+useCached
+  :: (P.Members '[KnitCache, P.Error PandocError] r, K.LogWithPrefixesLE r)
+  => CacheHolder r a
+  -> P.Sem r a
+useCached (Made x                     ) = return x
+useCached (RetrieveOrMake key action f) = f <$> retrieveOrMake key action
 
 mapLeft :: (a -> b) -> Either a c -> Either b c
 mapLeft f = either (Left . f) Right
@@ -58,40 +81,53 @@ cerealStrict = C.Serialize S.encode (mapLeft PandocSomeError . S.decode)
 cerealLazy :: S.Serialize a => C.Serialize PandocError a BL.ByteString
 cerealLazy = C.Serialize S.encodeLazy (mapLeft PandocSomeError . S.decodeLazy)
 
--- | Store an a (serialized to a strict ByteString) at key k. Throw PandocIOError on IOError.
-knitStore :: (K.KnitEffects r, S.Serialize a) => T.Text -> a -> P.Sem r ()
-knitStore k a = K.wrapPrefix "knitStore" $ do
+-- | Store an @a@ (serialized to a strict @ByteString@) at key k. Throw PandocIOError on IOError.
+store
+  :: ( P.Members '[KnitCache, P.Error PandocError] r
+     , K.LogWithPrefixesLE r
+     , S.Serialize a
+     )
+  => T.Text
+  -> a
+  -> P.Sem r ()
+store k a = K.wrapPrefix "knitStore" $ do
   K.logLE K.Diagnostic $ "Called with k=" <> k
   P.mapError ioErrorToPandocError $ C.store cerealStrict k a
 
 -- | Retrieve an a from the store at key k. Throw if not found or IOError
-knitRetrieve :: (K.KnitEffects r, S.Serialize a) => T.Text -> P.Sem r a
-knitRetrieve k = P.mapError ioErrorToPandocError $ C.retrieve cerealStrict k
+retrieve
+  :: (P.Members '[KnitCache, P.Error PandocError] r, S.Serialize a)
+  => T.Text
+  -> P.Sem r a
+retrieve k = P.mapError ioErrorToPandocError $ C.retrieve cerealStrict k
 
 -- | Retrieve an a from the store at key k.
 -- If retrieve fails then perform the action and store the resulting a at key k. 
-knitRetrieveOrMake
+retrieveOrMake
   :: forall a r
-   . (K.KnitEffects r, S.Serialize a)
+   . ( P.Members '[KnitCache, P.Error PandocError] r
+     , K.LogWithPrefixesLE r
+     , S.Serialize a
+     )
   => T.Text
   -> P.Sem r a
   -> P.Sem r a
-knitRetrieveOrMake k toMake = K.wrapPrefix "knitRetrieveOrMake" $ do
+retrieveOrMake k toMake = K.wrapPrefix "knitRetrieveOrMake" $ do
   ma <- C.retrieveMaybe cerealStrict k
   case ma of
     Nothing -> do
       K.logLE K.Diagnostic $ k <> " not found in cache. Making..."
       a <- toMake
-      knitStore k a
+      store k a
       K.logLE K.Diagnostic $ "Asset Stored."
       return a
     Just a -> do
       K.logLE K.Diagnostic $ k <> " found in cache."
       return a
 
--- | Clear the a stored at key k.
-knitClear :: K.KnitEffects r => T.Text -> P.Sem r ()
-knitClear k = P.mapError ioErrorToPandocError $ C.clear k
+-- | Clear the @b@ stored at key k.
+clear :: K.KnitEffects r => T.Text -> P.Sem r ()
+clear k = P.mapError ioErrorToPandocError $ C.clear k
 
 
 ioErrorToPandocError :: IE.IOError -> PandocError
