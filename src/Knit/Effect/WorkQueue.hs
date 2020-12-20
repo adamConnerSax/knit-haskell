74bebf02b271773f5310af6da157f7794faa8872
adamConnerSax <adam_conner_sax@yahoo.com>
2020-12-19 21:01:09 -0500

    Working on WorkQueue.  AsyncExample works.

--- src/Knit/Effect/WorkQueue.hs
+++ src/Knit/Effect/WorkQueue.hs
@@ -51,20 +51,20 @@ simpleJob x = Job x 1
 asyncWithQueue :: (P.Member (P.Embed IO) r, P.Member WorkQueue r, P.Member PA.Async r) => Job r a -> P.Sem r (Async.Async (Maybe a))
 asyncWithQueue (Job j n) = do
   (q, numCapabilities) <- PS.get
-  P.embed $ putStrLn $ "In asyncWithQueue (numCapabilities=" ++ show numCapabilities ++ ")"
-  P.embed $ putStrLn $ "writing () to queue to request use of cores."  
+--  P.embed $ putStrLn $ "In asyncWithQueue (numCapabilities=" ++ show numCapabilities ++ ")"
+--  P.embed $ putStrLn $ "writing () to queue to request use of cores."  
   capabilitiesToUse <- P.embed $ STM.atomically $ do
 --    used <- STM.lengthTBQueue q
     let toUse = min n numCapabilities
     _ <- traverse (STM.writeTBQueue q) $ replicate toUse () -- this blocks until there are enough/all are available
     return toUse
-  P.embed $ putStrLn $ "succeeded in writing " ++ show capabilitiesToUse ++ " entries to Q"
+--  P.embed $ putStrLn $ "succeeded in writing " ++ show capabilitiesToUse ++ " entries to Q"
   let jobWithQ = do
         a <- j
         _ <- P.embed
              $ STM.atomically             
              $ traverse (\_ -> STM.tryReadTBQueue q) $ replicate capabilitiesToUse () -- "tryRead" here in case numbers get out of whack
-        P.embed $ putStrLn $ "reading " ++ show capabilitiesToUse ++ " entries from queue to release cores."  
+--        P.embed $ putStrLn $ "reading " ++ show capabilitiesToUse ++ " entries from queue to release cores."  
         return a
   PA.async jobWithQ
 {-# INLINEABLE asyncWithQueue #-}  
